21eeb5a716f113b99baa53f875c2969098011317
Kostya <Rudnev@iMac-Kostya.local>
2015-12-11 17:19:14 +0300

    mobile version site completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 var mainMenu = document.getElementsByClassName('main-menu');
-var mainMenuBtn = document.getElementsByClassName('main-menu-bar__link');
+var mainMenuOpenBtn = document.getElementsByClassName('main-menu-bar__link');
 var mainMenuCloseBtn = document.getElementsByClassName('main-menu__close');
 
-
-mainMenuBtn.item('').addEventListener("click", function(event){
-	event.preventDefault();
-	mainMenu.item('').classList.add("main-menu--show");
+mainMenuOpenBtn.item('').addEventListener("click", function(event) {
+  "use strict";
+  event.preventDefault();
+  mainMenu.item('').classList.add("main-menu--show");
 });
 
-mainMenuCloseBtn.item('').addEventListener("click", function(event){
-	event.preventDefault();
-	mainMenu.item('').classList.remove("main-menu--show");
+mainMenuCloseBtn.item('').addEventListener("click", function(event) {
+  "use strict";
+  event.preventDefault();
+  mainMenu.item('').classList.remove("main-menu--show");
 });
