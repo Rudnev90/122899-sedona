db1796f95338b6207889e6cf57e4ebe2f51f5ec6
Kostya <Rudnev@iMac-Kostya.local>
2015-12-16 15:32:39 +0300

    tablet version site completed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-var mainMenu = document.getElementsByClassName('main-menu');
-var mainMenuOpenBtn = document.getElementsByClassName('main-menu-bar__link');
+var mainMenu = document.getElementsByClassName('main-menu__list');
+var mainMenuOpenBtn = document.getElementsByClassName('main-menu__open-link');
 var mainMenuCloseBtn = document.getElementsByClassName('main-menu__close');
 
 mainMenuOpenBtn.item('').addEventListener("click", function(event) {
-  "use strict";
   event.preventDefault();
   mainMenu.item('').classList.add("main-menu--show");
 });
 
 mainMenuCloseBtn.item('').addEventListener("click", function(event) {
-  "use strict";
   event.preventDefault();
   mainMenu.item('').classList.remove("main-menu--show");
 });
