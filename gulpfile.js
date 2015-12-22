9622c0609ff61e9d2b645d8b2e697ac9ada182a6
Konstantin Rudnev <rudnev.kl@live.ru>
2015-12-22 20:39:27 +0300

    modified gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -63,6 +63,12 @@ gulp.task("img", function() {
     .pipe(gulp.dest('build/img/'));
 });
 
+gulp.task('watch', function() {
+  gulp.watch("source/sass/style.{sass,scss}/**/*", ["css"])
+  gulp.watch("source/js/*.js", ["js"])
+  gulp.watch("source/img/", ["img"])
+})
+
 // =====================================================
 // Start task
 // =====================================================
