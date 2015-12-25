d26220aa66254146f3ff523e3788cf7e7001603c
Konstantin Rudnev <rudnev.kl@live.ru>
2015-12-25 15:19:28 +0300

    Fixxxx

--- source/js/script-menu.js
+++ source/js/script-menu.js
@@ -6,18 +6,15 @@
     var mainMenu = document.getElementsByClassName("main-menu__list");
     var mainMenuOpenBtn = document.getElementsByClassName("main-menu__open-link");
     var mainMenuCloseBtn = document.getElementsByClassName("main-menu__close");
+
     mainMenuOpenBtn.item("").addEventListener("click", function(event) {
       event.preventDefault();
-      if (document.getElementsByClassName("main-menu__list").item(' ').className == 'main-menu__list main-menu--show') {
-        mainMenu.item("").classList.remove("main-menu--show");
-      } else {
-        mainMenu.item("").classList.add("main-menu--show");
-      }
+        mainMenu.item("").classList.remove("main-menu--close");
     });
 
     mainMenuCloseBtn.item("").addEventListener("click", function(event) {
       event.preventDefault();
-      mainMenu.item("").classList.remove("main-menu--show");
+      mainMenu.item("").classList.add("main-menu--close");
     });
   }, 1000);
 
